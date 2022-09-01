e77fb70dd23f6c1d5a226527120977ace385c127
Boris Chirino <bolek1976@yahoo.com>
2022-09-01 16:16:59 +0200

    Initial Commit

---
+++ VideClub/VideClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideClubApp.swift
+//  VideClub
+//
+//  Created by Boris Chirino on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct VideClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
